d3ab234aee26a9b834ba128c8d44419db16882b2
LucasA75 <lucasacevedocastro@gmail.com>
2023-12-12 16:47:12 -0300

    add bcrypt

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet')
-
+const bCrispt = require('bcrypt')
+b
 /* 
 app.use(helmet.hidePoweredBy())
 app.use(helmet.frameguard({ action: 'deny' }))
